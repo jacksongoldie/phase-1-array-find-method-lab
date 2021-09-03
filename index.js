05e38c37cfb205292c52f3c9b66851cdcf0f0b4e
Goldie <goldiejackson25@yahoo.com>
2021-09-03 18:03:57 -0500

    need help with returning year

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+/* 
+function superbowlWin(anArray){
+    anArray.find(isWin);
+}
 
+function isWin(recordArray){
+    recordArray.result === "W" ? recordArray.year : undefined;
+} */
+
+
+//called with superbowlWin(record)
+function superbowlWin(recordArray){
+   if (recordArray.find(element => element.result === "W")){
+       return recordArray.year;
+   };
+}
