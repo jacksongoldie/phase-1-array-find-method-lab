1cc62e793d43ba5be5f8dfcd4a780881b7f95925
Goldie <goldiejackson25@yahoo.com>
2021-09-04 07:24:03 -0500

    tests passing

--- index.js
+++ index.js
@@ -8,9 +8,21 @@ function isWin(recordArray){
 } */
 
 
-//called with superbowlWin(record)
+//called with superbowlWin(record) - does not work because recordArray.year is undefined; recordArray is left nondestructed so we can't call on the .year because it doesn't know which one
 function superbowlWin(recordArray){
    if (recordArray.find(element => element.result === "W")){
        return recordArray.year;
    };
 }
+
+function superbowlWin(recordArray){
+    //sets win to an object {year: and result:}, recordArray stays the same
+    let win = recordArray.find(element => element.result === "W");
+    debugger;
+    if(win != undefined){
+    return win.year;
+    }
+    else{
+        return undefined;
+    }
+ }
